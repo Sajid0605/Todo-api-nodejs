9c317686006e8ff31c9ddff478e528b8b19be3d7
Sajid Ansari <sajidansari0605@gmail.com>
2018-10-01 15:48:15 +0530

    Mongo Update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,47 @@
+const {MongoClient, ObjectID} = require('mongodb');
+const test = require('assert');
+
+const url = 'mongodb://127.0.0.1:27017';
+// Database Name
+const dbName = 'TodoApp';
+
+// Connect using MongoClient
+MongoClient.connect(url, function(err, client) {
+    if(err){
+        return console.log('Unable to connect Database Server');
+    }
+    console.log('Connect to MongoDB server');
+
+    // Create a collection we want to drop later
+    // const col = client.db(dbName).collection('Todos');
+    const col = client.db(dbName).collection('Users');
+
+    // col.findOneAndUpdate({
+    //     _id: new ObjectID('5bb1b8a92170e3b316bcfbbd'),
+    // },{
+    //     $set:{
+    //             completed: true
+    //     }
+    // },{
+    //     returnOriginal: false
+    // }).then((res) => {
+    //     console.log(res);
+    // });
+
+    col.findOneAndUpdate({
+        _id: new ObjectID('5bb1b8a92170e3b316bcfbbd'),
+    },{
+        $set:{
+            name: 'Mohammad Sajid Ansari'
+        },
+        $inc:{
+            age: 1
+        }
+    },{
+        returnOriginal: false
+    }).then((res) => {
+        console.log(res);
+    })
+
+
+});
