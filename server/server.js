bcd34d73c4aebc1943a9beab2d134fe25b41f2a6
Sajid Ansari <sajidansari0605@gmail.com>
2018-10-02 10:36:08 +0530

    get todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/config');
 const {Todo} = require('./models/todo');
@@ -22,11 +23,31 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-       res.send({todos});
+   Todo.find().then((todo) => {
+       res.send({todo});
    }, (err) => {
         res.status(400).send(err);
    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+
+            return res.status(404).send(todo);
+        }
+
+        res.send({todo});
+
+    }, (err) => {
+        res.status(400).send({err});
+    })
 });
 
 app.listen(3010, () => {
